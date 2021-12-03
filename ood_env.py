8b980b8734a69d9a4b64be60d3015422b0975c24
Shubham Jha <jha1shubham@gmail.com>
2021-12-03 02:42:09 -0500

    Implement task shift

--- ood_env.py
+++ ood_env.py
@@ -4,7 +4,7 @@ import numpy as np
 from gym.core import ObservationWrapper
 
 
-def generate_ood(ood_config, state=None):
+def generate_ood(ood_config, outlier_envs, state=None):
     '''
     Generates OOD state based on the type and base state (optional)
 
@@ -14,8 +14,10 @@ def generate_ood(ood_config, state=None):
         if state is None:
             raise Exception("Can't add random noise without initial state")
         return np.clip(state + np.random.normal(0, ood_config.random_std, state.shape), a_min=0, a_max=255).astype(int)
-    else: #TODO
-        raise NotImplementedError
+    elif ood_config.type == 'task':
+        return random.choice(outlier_envs).render(mode='rgb_array')
+    else:  # TODO
+        pass
 
 class OODEnv(ObservationWrapper):
     '''
@@ -26,8 +28,9 @@ class OODEnv(ObservationWrapper):
         super().__init__(base_env)
         self.base_env = base_env
         self.ood_config = ood_config
+        self.outlier_envs = [gym.make(outlier_env_name) for outlier_env_name in ood_config.outlier_env_names] # TODO: Handle exception
 
     def observation(self, observation=None):
         if random.random() < self.ood_config.prob:
-            observation = generate_ood(self.ood_config, observation)
+            observation = generate_ood(self.ood_config, self.outlier_envs, observation)
         return observation
